1e5374cb6e2a926f95217667e9f384f956765aea
englianhu <englianhu@gmail.com>
2016-04-30 13:45:24 -0400

    quiz

---
+++ Data Visualization/Quiz/quiz-w1.R
@@ -0,0 +1,42 @@
+https://www.coursera.org/learn/datavisualization/exam/GpPq3/week-1-quiz
+
+Answer of Quiz:
+
+Q1 : Which kind of visualization would you use to try to answer a personal question about your data?
+Ans : Interactive visualization
+
+Q2 : Which kind of visualization would you use to share a discovery about your data with your colleagues in a slide show?
+Ans : Presentation visualization
+
+Q3 : Which kind of visualization would you use to create a webpage that allows viewers to see a visualization of data that you prepared, but also allows the viewer to further investigate the data?
+Ans : Interactive storytelling
+
+Q4 : In what order does a data visualization graphics pipeline process information?
+Pixel processing, then rasterization, then vertex processing wrong
+Ans : Vertex processing, then rasterization, then pixel processing
+
+Q5 : How many items can human working memory (short-term memory) typically hold?
+Ans : 3–7 items
+
+Q6 : A light gray box drawn on top of a dark gray background will make the light gray box appear ______________.
+Ans : Brighter
+
+Q7 : When visualizing data, you should keep your eyes focused on one point for the entire duration of the visualization.
+Ans : False, because your visual system will play tricks on your perception of the data.
+
+Q8 : On which of these colors does the human eye have the most difficulty focusing?
+Ans : Blue
+
+Q9 : Which one of the 3-D depth cues below indicates surface orientation?
+ Stereopsis, Occlusion - wrong
+Q9 : Which one of the 3-D depth cues below is the strongest?
+Ans : Occlusion
+
+Q10 : Given a plot of life expectancy based on country and birth year, you look up your country and birth year and find the displayed life expectancy, and conclude you will probably live that long. This is an example of _________________.
+Inductive reasoning, Abductive reasoning - wrong
+Ans : 
+
+
+
+
+
